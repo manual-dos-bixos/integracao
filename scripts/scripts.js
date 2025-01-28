91a08e3426e50eee9a965bd4418f74c806d72b83
manual-dos-bixos <manualdosbixos.fatec@gmail.com>
2025-01-28 09:45:06 -0300

    Atualizar o scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 $(document).on('click', '#scroll-to-main', function() {
     $('body').css({'overflow': 'scroll'});
     window.scrollTo({
-        top: window.innerHeight, 
+        top: $('#main').offset().top, 
         behavior: 'smooth'
     });
 });
