6f61360b790d61cd4b8e23b3b890644654d582d2
Letícia Gama <letcgam@gmail.com>
2025-01-31 23:47:17 -0300

    Visual e funcionamento de progresso do form de inscrição, sections de inputs

--- static/scripts/form.js
+++ static/scripts/form.js
@@ -1,12 +1,13 @@
-
 $(document).ready(function() {
+    $('#btn-next, #btn-prev').hide();
+
     // TROCAR RADIO BUTTON POR BOTAO
     $('input.form-check').each(function() {
         let id = $(this).prop('id');
 
         $('#curso-btns').append(`<button class="btn btn-light btn-curso" id="btn-${id}">${$(`label[for="${id}"]`).text()}</button>`);
         $(this).parent().hide();
-    })
+    });
 });
 
 $(document).on('click', '.btn-curso', function(e) {
@@ -20,37 +21,68 @@ $(document).on('click', '.btn-curso', function(e) {
     radio.prop('checked', true);
 });
 
-$(document).on('click', '#btn-next', function(e) {
+$(document).on('click', '#btn-prev, #btn-next', function(e) {
+    let tipoAluno = $('.btn-inscricao.selected-btn').data('tipo');
+    let numEtapas = tipoAluno == 'calouro' ? $('.form-section').length - 1 : $('.form-section').length;
+
     let sectionAtual = $('.section-atual');
-    let nextSection = sectionAtual.next();
 
-    sectionAtual.removeClass('section-atual').css('left', '-100%');
-    nextSection.addClass('section-atual').css('left', '0');
+    if ($(this).prop('id') == 'btn-next') {
+        let nextSection = sectionAtual.next();
 
-    $('#btn-prev').removeClass('disabled');
-    if (nextSection.is('.form-section:last-child')) $(this).addClass('disabled');
-});
+        sectionAtual.removeClass('section-atual').css('left', '-100%');
+        nextSection.addClass('section-atual').css('left', '0');
 
-$(document).on('click', '#btn-prev', function(e) {
-    let sectionAtual = $('.section-atual');
-    let prevSection = sectionAtual.prev();
+        $('#btn-prev').removeClass('disabled');
+        if ($('.form-section.section-atual').index() == numEtapas - 1) $(this).addClass('disabled');
+    } else {
+        let prevSection = sectionAtual.prev();
+        sectionAtual.removeClass('section-atual').css('left', '100%');
+        prevSection.addClass('section-atual').css('left', '0');
 
-    sectionAtual.removeClass('section-atual').css('left', '100%');
-    prevSection.addClass('section-atual').css('left', '0');
+        $('#btn-next').removeClass('disabled');
+        if (prevSection.is('.form-section:first-child')) $(this).addClass('disabled');
+    }
 
-    $('#btn-next').removeClass('disabled');
-    if (prevSection.is('.form-section:first-child')) $(this).addClass('disabled');
+    let etapaAtual = $('.section-atual').index();
+    $('#form-progress-bar .progress').css({'width': (etapaAtual / (numEtapas - 1) * $('#form-progress-bar').width()) + 'px'});
+    $('.step-icon').slice(0, etapaAtual + 1).css({'background-color': '#641290', 'color': '#fff'})
+    $('.step-icon').slice(etapaAtual + 1).css({'background-color': '#fff', 'color': '#641290'})
 });
 
 $(document).on('click', '.btn-inscricao', function(e) {
     e.preventDefault();
+    $('.btn-inscricao').removeClass('selected-btn');
+    $(this).addClass('selected-btn');
 
     let tipoAluno = $(this).data('tipo');
-    console.log(tipoAluno);
+    let numEtapas = tipoAluno == 'calouro' ? $('.form-section').length - 1 : $('.form-section').length;
+
+    if ($('#form-progress-bar').css('opacity') == 1) {
+        $('#form-progress-bar .step-icon').animate({opacity: 0}, 100);
+        setTimeout(() => {
+            $('#form-progress-bar .step-icon').remove();
+            
+            for (let i = 0; i < numEtapas; i++) $('#form-progress-bar').append(`<span class="step-icon">${i + 1}</i>`);
+            $('#form-progress-bar .step-icon').css({'opacity': 0});
+            $('#form-progress-bar .step-icon').animate({opacity: 1}, 100);
+            $('.step-icon').first().css({'background-color': '#641290', 'color': '#fff'});
+        }, 100);
+    } else {
+        for (let i = 0; i < numEtapas; i++) $('#form-progress-bar').append(`<span class="step-icon">${i + 1}</i>`);
+        $('#form-progress-bar .step-icon').css({'opacity': 0});
+        $('#form-progress-bar').animate({opacity: 1}, 100);
+        $('#form-progress-bar .step-icon').animate({opacity: 1}, 100);
+        $('.step-icon').first().css({'background-color': '#641290', 'color': '#fff'})
+    }
+
+    if ($('#btn-next').is(':hidden')) setTimeout(() => $('#btn-next').click(), 600);
+    $('#btn-next, #btn-prev').prop('hidden', false);
+    $('#btn-next, #btn-prev').fadeIn(100);
+
 });
 
 // FORMATAR TELEFONE
-
 // Aplica a formatação conforme a quantidade de caracteres
 $(document).on('input', '.telefone-input', function() {
     var value = $(this).val().replace(/\D/g, '');
